b0a80d1b39aa274399ad9468a9d8d5cc81b522e5
Hyoungwoo <dlguddn3024@gmail.com>
2023-02-06 17:22:13 +0900

    Feature: Model 추가, 의존성 제거 구현

--- study/servlet/servlet/src/main/java/hello/servlet/web/frontcontroller/v3/ControllerV3.java
+++ study/servlet/servlet/src/main/java/hello/servlet/web/frontcontroller/v3/ControllerV3.java
@@ -2,7 +2,9 @@ package hello.servlet.web.frontcontroller.v3;
 
 import hello.servlet.web.frontcontroller.ModelView;
 
+import java.util.Map;
+
 public interface ControllerV3 {
 
-    ModelView process
+    ModelView process(Map<String, String> paramMap);
 }
