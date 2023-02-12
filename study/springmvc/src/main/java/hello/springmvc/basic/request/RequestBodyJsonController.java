0e1d0756a100778f4a608ec264e3ff4a3bb2c082
Hyoungwoo <dlguddn3024@gmail.com>
2023-02-12 18:15:01 +0900

    Feature: 오늘 공부 끝~

---
+++ study/springmvc/src/main/java/hello/springmvc/basic/request/RequestBodyJsonController.java
@@ -0,0 +1,30 @@
+package hello.springmvc.basic.request;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * {"username":"hello", "age":20}
+ * content-type: application/json
+ */
+@Slf4j
+@Controller
+public class RequestBodyJsonController {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @PostMapping("/request-body-json-v1")
+    public void requestBodyJsonV1(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        ServletInputStream inputStream = request.getInputStream();
+        StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8)
+    }
+}
