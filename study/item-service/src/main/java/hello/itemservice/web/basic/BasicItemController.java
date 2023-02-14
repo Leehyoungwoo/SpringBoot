da121cd569f07f53c1515c2f1b4460d30c2d2def
Hyoungwoo <dlguddn3024@gmail.com>
2023-02-14 16:09:14 +0900

    Feature: 상품 등록 폼 구현

--- study/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ study/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -3,10 +3,10 @@ package hello.itemservice.web.basic;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.PostConstruct;
@@ -15,7 +15,7 @@ import java.util.List;
 @Controller
 @RequestMapping("/basic/items")
 @RequiredArgsConstructor
-public class basicItemController {
+public class BasicItemController {
 
     private final ItemRepository itemRepository;
 
@@ -26,11 +26,24 @@ public class basicItemController {
         return "basic/items";
     }
 
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model){
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+    @GetMapping("add")
+    public String addForm() {
+        return "basic/addForm"
+    }
+
     /**
      * 테스트용 데이터 추가
      */
     @PostConstruct
     public void inint() {
+
         itemRepository.save(new Item("itemA", 10000, 10));
         itemRepository.save(new Item("itemB", 20000, 20));
     }
